1077d0fd9df8469f9f4ced751e7bed36bdfd2000
Caio Geraldes <caio.geraldes@usp.br>
2023-03-29 11:19:01 -0300

    astronvim e tmux

---
+++ nvim/.config/nvim/lua/user/plugins/user.lua
@@ -0,0 +1,29 @@
+return {
+  "rust-lang/rust.vim",
+  "simrat39/rust-tools.nvim",
+  "tpope/vim-surround",
+  "tpope/vim-repeat",
+  'lervag/vimtex',
+  "sainnhe/sonokai",
+  "sainnhe/gruvbox-material",
+  "jamespeapen/Nvim-R",
+  "eigenfoo/stan-vim",
+  "hrsh7th/cmp-nvim-lsp-signature-help",
+  "hrsh7th/cmp-calc",
+  "hrsh7th/cmp-emoji",
+  "f3fora/cmp-spell",
+  "ray-x/cmp-treesitter",
+  "kdheepak/cmp-latex-symbols",
+  "jc-doyle/cmp-pandoc-references",
+  "onsails/lspkind-nvim",
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+}
