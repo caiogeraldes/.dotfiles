576439aa09f0337c175bda2758cc0df314b3f29d
Caio Geraldes <caio.geraldes@usp.br>
2023-09-20 10:31:30 -0300

    kos, ts, format

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -16,52 +16,62 @@ return {
             --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
         }
     },
-  -- Configure require("lazy").setup() options
-  lazy = {
-    defaults = { lazy = true },
-    performance = {
-      rtp = {
-        -- customize default disabled vim plugins
-        disabled_plugins = {
-          "tohtml", "gzip", "matchit", "zipPlugin", "tarPlugin"
     -- Set colorscheme to use
     colorscheme = "gruvbox-material",
+    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+    diagnostics = {virtual_text = true, underline = true},
+    lsp = {
+        formatting = {
+            format_on_save = {
+                enabled = true,
+                allow_filetypes = {"rust", "lua", "markdown"},
+                ignore_filetypes = {}
+            },
+            disabled = {},
+            timeout_ms = 1000 -- default format timeout
+        },
+        servers = {},
+        config = {texlab = {build = {onSave = true}}}
+    },
+    -- Configure require("lazy").setup() options
+    lazy = {
+        defaults = {lazy = true},
+        performance = {
+            rtp = {
+                -- customize default disabled vim plugins
+                disabled_plugins = {
+                    "tohtml", "gzip", "matchit", "zipPlugin", "tarPlugin"
+                }
+            }
         }
-      }
-    }
-  },
-  -- This function is run last and is a good place to configuring
-  -- augroups/autocommands and custom filetypes also this just pure lua so
-  -- anything that doesn't fit in the normal config locations above can go here
-  polish = function()
-    -- Latex
-    vim.api.nvim_create_augroup("tex", { clear = true })
-    vim.api.nvim_create_autocmd("FileType", {
-      group = "tex",
-      callback = function()
-        vim.opt.wrap = true
-        vim.opt.linebreak = true
-        vim.opt.breakindent = true
-      end
-    })
-    -- Spell
-    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-      pattern = { "*.tex", "*.md", "*.Rmd" },
-      command = "set spell spelllang=pt,en,grc,de"
-    })
-    require("notify").setup { background_colour = "#000000" }
+    },
+    -- This function is run last and is a good place to configuring
+    -- augroups/autocommands and custom filetypes also this just pure lua so
+    -- anything that doesn't fit in the normal config locations above can go here
+    polish = function()
+        -- Latex
+        vim.api.nvim_create_augroup("tex", {clear = true})
+        vim.api.nvim_create_autocmd("FileType", {
+            group = "tex",
+            callback = function()
+                vim.opt.wrap = true
+                vim.opt.linebreak = true
+                vim.opt.breakindent = true
+            end
+        })
+        -- Spell
+        vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+            pattern = {"*.tex"},
+            command = "set spell spelllang=pt,en,grc,de"
+        })
+        vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+            pattern = {"*.md", "*.Rmd"},
+            command = "set spell spelllang=pt,grc"
+        })
 
-    -- Set up custom filetypes
-    -- vim.filetype.add {
-    --   extension = {
-    --     foo = "fooscript",
-    --   },
-    --   filename = {
-    --     ["Foofile"] = "fooscript",
-    --   },
-    --   pattern = {
-    --     ["~/%.config/foo/.*"] = "fooscript",
-    --   },
-    -- }
-  end
+        require("notify").setup {background_colour = "#000000"}
+
+        -- Set up custom filetypes
+        vim.filetype.add {extension = {ks = "kerboscript"}}
+    end
 }
--- nvim/.config/nvim/lua/user/plugins/user.lua
+++ nvim/.config/nvim/lua/user/plugins/user.lua
@@ -1,30 +1,29 @@
 return {
-  "rust-lang/rust.vim",
-  "simrat39/rust-tools.nvim",
-  "tpope/vim-surround",
-  "tpope/vim-repeat",
-  "lervag/vimtex",
-  "sainnhe/sonokai",
-  "sainnhe/gruvbox-material",
-  "jamespeapen/Nvim-R",
-  "eigenfoo/stan-vim",
-  "hrsh7th/cmp-nvim-lsp-signature-help",
-  "hrsh7th/cmp-calc",
-  "hrsh7th/cmp-emoji",
-  "f3fora/cmp-spell",
-  "ray-x/cmp-treesitter",
-  "kdheepak/cmp-latex-symbols",
-  "jc-doyle/cmp-pandoc-references",
-  "onsails/lspkind-nvim",
-  "eigenfoo/stan-vim",
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+    "rust-lang/rust.vim", "simrat39/rust-tools.nvim", "tpope/vim-repeat",
+    "lervag/vimtex", "sainnhe/sonokai", {"sainnhe/gruvbox-material"},
+    "jamespeapen/Nvim-R", "eigenfoo/stan-vim",
+    "hrsh7th/cmp-nvim-lsp-signature-help", "hrsh7th/cmp-calc",
+    "hrsh7th/cmp-emoji", "f3fora/cmp-spell", "ray-x/cmp-treesitter",
+    "kdheepak/cmp-latex-symbols", "jc-doyle/cmp-pandoc-references",
+    "onsails/lspkind-nvim", "eigenfoo/stan-vim", "Freedzone/kerbovim",
+    "yump/vim-kerboscript", {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup {
+                -- Configuration here, or leave empty to use defaults
+            }
+        end
+    }
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
 }
