9657db836d466e2a2d60a3f1a6d5af7d2d368258
Caio Geraldes <caio.geraldes@usp.br>
2023-09-20 10:30:42 -0300

    gruvbox vim

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -1,38 +1,21 @@
 return {
-  -- Configure AstroNvim updates
-  updater = {
-    remote = "origin",         -- remote to use
-    channel = "stable",        -- "stable" or "nightly"
-    version = "latest",        -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly",        -- branch name (NIGHTLY ONLY)
-    commit = nil,              -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil,         -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false,      -- skip prompts about breaking changes
-    show_changelog = true,     -- show the changelog after performing an update
-    auto_quit = false,         -- automatically quit the current session after a successful update
-    remotes = {                -- easily add new remotes to track
-      --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
-      --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
-      --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
-    }
-  },
-  -- Set colorscheme to use
-  colorscheme = "gruvbox-material",
-  -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-  diagnostics = { virtual_text = true, underline = true },
-  lsp = {
-    formatting = {
-      format_on_save = {
-        enabled = true,
-        allow_filetypes = { "rust", "lua" },
-        ignore_filetypes = {}
-      },
-      disabled = {},
-      timeout_ms = 1000       -- default format timeout
+    -- Configure AstroNvim updates
+    updater = {
+        remote = "origin",
+        channel = "stable",
+        version = "latest",
+        branch = "nightly", -- branch name (NIGHTLY ONLY)
+        commit = nil, -- commit hash (NIGHTLY ONLY)
+        pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
+        skip_prompts = false, -- skip prompts about breaking changes
+        show_changelog = true, -- show the changelog after performing an update
+        auto_quit = false, -- automatically quit the current session after a successful update
+        remotes = { -- easily add new remotes to track
+            --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
+            --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
+            --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
+        }
     },
-    servers = {},
-    config = { texlab = { build = { onSave = true } } }
-  },
   -- Configure require("lazy").setup() options
   lazy = {
     defaults = { lazy = true },
@@ -41,6 +24,8 @@ return {
         -- customize default disabled vim plugins
         disabled_plugins = {
           "tohtml", "gzip", "matchit", "zipPlugin", "tarPlugin"
+    -- Set colorscheme to use
+    colorscheme = "gruvbox-material",
         }
       }
     }
