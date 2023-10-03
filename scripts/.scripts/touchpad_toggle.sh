57f797a030001b45815530c40e5e233701473b28
Caio Geraldes <caio.geraldes@usp.br>
2023-10-03 18:39:38 -0300

    volume, touchpad

---
+++ scripts/.scripts/touchpad_toggle.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+script_dir=$HOME/.scripts/
+
+. ${script_dir}/touchpad_id.sh
+
+$(get_touchpad_id)
+TOUCHPAD_ID=$?
+
+#echo TOUCHPAD_ID=$TOUCHPAD_ID
+
+TOUCHPAD_ENABLED=$(xinput list-props ${TOUCHPAD_ID} | grep Enabled | sed 's/.*\(.\)$/\1/')
+
+#echo ENABLED=${TOUCHPAD_ENABLED}
+
+if [ ${TOUCHPAD_ENABLED} -eq 1 ]; then
+    xinput disable ${TOUCHPAD_ID}
+else
+    xinput enable ${TOUCHPAD_ID}
+fi
